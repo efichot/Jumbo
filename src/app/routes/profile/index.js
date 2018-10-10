7ea0b56b7ffcabe4f97ae82aade822ea2b7732f3
efichot <fichotetienne@gmail.com>
2018-10-10 19:09:14 +0200

    recharts added

--- src/app/routes/profile/index.js
+++ src/app/routes/profile/index.js
@@ -7,19 +7,36 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import Select from '@material-ui/core/Select';
-import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
+import TextField from '@material-ui/core/TextField';
 import Foot from 'assets/images/products/football.jpeg';
 import iPhone from 'assets/images/products/iPhone.jpeg';
 import { functions } from 'helper/firebase';
 import { NotificationManager } from 'react-notifications';
+import {
+  Area,
+  AreaChart,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis
+} from 'recharts';
 
 export class Profile extends Component {
   state = {
     topic: '',
-    loader: false
+    loader: false,
+    chartData: [
+      { name: 'Jan', Income: 10, Expanse: 22 },
+      { name: 'Feb', Income: 5, Expanse: 20 },
+      { name: 'March', Income: 15, Expanse: 26 },
+      { name: 'April', Income: 13, Expanse: 22 },
+      { name: 'May', Income: 35, Expanse: 26 },
+      { name: 'June', Income: 25, Expanse: 20 },
+      { name: 'July', Income: 20, Expanse: 32 },
+      { name: 'Aug', Income: 25, Expanse: 35 }
+    ]
   };
 
   render() {
@@ -44,7 +61,9 @@ export class Profile extends Component {
             <div className="col-12 col-md-5 mb-3">
               <Card>
                 <CardContent className="border-bottom border-grey-lighten-2 pb-0">
-                  <h3>Subscribe to topics</h3>
+                  <h3 className="d-flex justify-content-center">
+                    Subscribe to topics
+                  </h3>
                   <ul className="p-0">
                     {topics.map((topic, index) => (
                       <li
@@ -108,31 +127,30 @@ export class Profile extends Component {
                     ))}
                   </ul>
                 </CardContent>
-                <CardActions className="d-flex align-items-end">
-                  <FormControl className="w-50 ml-5">
-                    <InputLabel htmlFor="topics">Topics</InputLabel>
-                    <Select
-                      value={this.state.topic}
-                      onChange={e =>
-                        this.setState({ [e.target.name]: e.target.value })
-                      }
-                      inputProps={{
-                        name: 'topic',
-                        id: 'topics'
-                      }}
-                    >
-                      {topics.map((topic, index) => (
-                        <MenuItem key={index} value={topic.name}>
-                          {topic.name}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
+                <CardActions className="d-flex align-items-end mb-2">
+                  <TextField
+                    select
+                    label="Topics"
+                    value={this.state.topic}
+                    name="topic"
+                    onChange={e =>
+                      this.setState({ [e.target.name]: e.target.value })
+                    }
+                    variant="outlined"
+                    className="w-50 ml-5"
+                  >
+                    {topics.map((topic, index) => (
+                      <MenuItem key={index} value={topic.name}>
+                        {topic.name}
+                      </MenuItem>
+                    ))}
+                  </TextField>
                   <Button
                     variant="contained"
                     color="secondary"
                     className="jr-btn jr-btn-xs ml-auto"
                     onClick={() => {
+                      this.setState({ loader: true });
                       functions
                         .httpsCallable('sendPushMessageToTopic')({
                           topic: this.state.topic
@@ -144,6 +162,7 @@ export class Profile extends Component {
                           } else {
                             NotificationManager.error(message);
                           }
+                          this.setState({ loader: false });
                         })
                         .catch(e => NotificationManager.error(e));
                     }}
@@ -159,8 +178,42 @@ export class Profile extends Component {
             </div>
             <div className="col-12 col-md-7 mb-3">
               <Card>
-                <CardContent />
-                <CardActions />
+                <CardContent>
+                  <ResponsiveContainer width="100%" height={192}>
+                    <AreaChart
+                      data={this.state.chartData}
+                      margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
+                    >
+                      <Legend
+                        layout="horizontal"
+                        verticalAlign="top"
+                        margin={{ left: 10 }}
+                        wrapperStyle={{
+                          top: 0,
+                          left: 24,
+                          lineHeight: '24px'
+                        }}
+                      />
+                      <XAxis dataKey="name" />
+                      {/* <YAxis /> */}
+                      <Tooltip />
+                      <Area
+                        type="monotone"
+                        dataKey="Expanse"
+                        stroke="#f3b439"
+                        fillOpacity={0.8}
+                        fill="#f3b439"
+                      />
+                      <Area
+                        type="monotone"
+                        dataKey="Income"
+                        stroke="#03275b"
+                        fillOpacity={0.8}
+                        fill="#3367d6"
+                      />
+                    </AreaChart>
+                  </ResponsiveContainer>
+                </CardContent>
               </Card>
             </div>
           </div>
